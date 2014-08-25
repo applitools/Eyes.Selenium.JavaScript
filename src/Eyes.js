7f626a54d2852fda3db41b99df158e60e6d3a5f6
Daniel Puterman <daniel.puterman@applitools.com>
2014-08-25 20:33:52 +0300

    added setting deferredFactory

--- src/Eyes.js
+++ src/Eyes.js
@@ -38,17 +38,19 @@
 
     //noinspection JSUnusedGlobalSymbols
     Eyes.prototype._getBaseAgentId = function () {
-        return 'selenium-js/0.0.7';
+        return 'selenium-js/0.0.8';
     };
 
     Eyes.prototype.open = function (driver, appName, testName, viewportSize) {
         var flow = this._flow = driver.controlFlow();
-        PromiseFactory.setPromiseHandler(function (asyncAction) {
+        PromiseFactory.setFactoryMethods(function (asyncAction) {
             return flow.execute(function () {
                 var deferred = webdriver.promise.defer();
                 asyncAction(deferred.fulfill, deferred.reject);
                 return deferred.promise;
             });
+        }, function () {
+            return webdriver.promise.defer();
         });
         return this._flow.execute(function () {
             var deferred = webdriver.promise.defer();
