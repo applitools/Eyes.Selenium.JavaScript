e95bc5925461bcd467755101e52fc66468842b45
Daniel Puterman <daniel.puterman@applitools.com>
2014-10-19 23:13:23 +0300

    Using the updated SDK + updated agent ID

--- src/Eyes.js
+++ src/Eyes.js
@@ -38,7 +38,7 @@
 
     //noinspection JSUnusedGlobalSymbols
     Eyes.prototype._getBaseAgentId = function () {
-        return 'selenium-js/0.0.17';
+        return 'selenium-js/0.0.18';
     };
 
     Eyes.prototype.open = function (driver, appName, testName, viewportSize) {
