b20839fc2cf1080522d058fe17544fc14f1d42b1
Daniel Puterman <daniel.puterman@applitools.com>
2015-01-19 19:42:08 +0200

    Base SDK rev. up

--- src/Eyes.js
+++ src/Eyes.js
@@ -40,7 +40,7 @@
 
     //noinspection JSUnusedGlobalSymbols
     Eyes.prototype._getBaseAgentId = function () {
-        return 'selenium-js/0.0.23';
+        return 'selenium-js/0.0.24';
     };
 
     Eyes.prototype.open = function (driver, appName, testName, viewportSize) {
