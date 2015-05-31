c11a22adc5e764643258ac6f0d587d5258cd77a5
Doron Zavelevsky <DoronWorkMac@Dorons-MacBook-Pro.local>
2015-05-31 19:01:35 +0300

    agent id changed

--- src/Eyes.js
+++ src/Eyes.js
@@ -51,7 +51,7 @@
 
     //noinspection JSUnusedGlobalSymbols
     Eyes.prototype._getBaseAgentId = function () {
-        return 'selenium-js/0.0.31';
+        return 'selenium-js/0.0.35';
     };
 
     Eyes.prototype.open = function (driver, appName, testName, viewportSize) {
