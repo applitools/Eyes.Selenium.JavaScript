58d7890d45b3eba34c8dc9ad661245c4f9d21d0b
danielputerman <danielputerman@users.noreply.github.com>
2016-02-07 22:55:11 +0200

    Base SDK version up.

--- src/Eyes.js
+++ src/Eyes.js
@@ -52,7 +52,7 @@
 
     //noinspection JSUnusedGlobalSymbols
     Eyes.prototype._getBaseAgentId = function() {
-        return 'selenium-js/0.0.44';
+        return 'selenium-js/0.0.45';
     };
 
     function _init(that, flow) {
