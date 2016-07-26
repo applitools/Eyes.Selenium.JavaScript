9431b0466072b43293dd5163d48a81cff0f443ef
danielputerman <danielputerman@users.noreply.github.com>
2016-07-27 01:21:50 +0300

    Dependencies update + Version up.

--- src/Eyes.js
+++ src/Eyes.js
@@ -55,7 +55,7 @@
 
     //noinspection JSUnusedGlobalSymbols
     Eyes.prototype._getBaseAgentId = function() {
-        return 'selenium-js/0.0.49';
+        return 'selenium-js/0.0.50';
     };
 
     function _init(that, flow) {
