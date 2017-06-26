04845c72998ecf9eef330f3b8d9b92b4ec5715cd
Daniel Puterman <daniel.puterman@applitools.com>
2017-06-26 18:33:49 +0300

    Version up.

--- src/Eyes.js
+++ src/Eyes.js
@@ -85,7 +85,7 @@
 
     //noinspection JSUnusedGlobalSymbols
     Eyes.prototype._getBaseAgentId = function () {
-        return 'selenium-js/0.0.68';
+        return 'selenium-js/0.0.69';
     };
 
     function _init(that, flow) {
