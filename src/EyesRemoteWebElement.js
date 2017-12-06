3b2dbfb2176690a19fa53cbf0320878f29344cfb
Oleh Astappiev <astappev@gmail.com>
2017-12-06 21:16:13 +0100

    Fix for stuck problem with element's methods

--- src/EyesRemoteWebElement.js
+++ src/EyesRemoteWebElement.js
@@ -154,14 +154,14 @@
 
     EyesRemoteWebElement.prototype.findElement = function (locator) {
         var that = this;
-        return this._element.findElement(locator).then(function (element) {
+        return this.getRemoteWebElement().findElement(locator).then(function (element) {
             return new EyesRemoteWebElement(element, that._eyesDriver, that._logger);
         });
     };
 
     EyesRemoteWebElement.prototype.findElements = function (locator) {
         var that = this;
-        return this._element.findElements(locator).then(function (elements) {
+        return this.getRemoteWebElement().findElements(locator).then(function (elements) {
             return elements.map(function (element) {
                 return new EyesRemoteWebElement(element, that._eyesDriver, that._logger);
             });
@@ -174,7 +174,7 @@
      * @return {promise.Promise.<string>} The value of the style property of the element, or {@code null}.
      */
     EyesRemoteWebElement.prototype.getComputedStyle = function (propStyle) {
-        return this._eyesDriver.executeScript(JS_GET_COMPUTED_STYLE_FORMATTED_STR(propStyle), this._element);
+        return this._eyesDriver.executeScript(JS_GET_COMPUTED_STYLE_FORMATTED_STR(propStyle), this.getRemoteWebElement());
     };
 
     /**
@@ -190,7 +190,7 @@
      * @return {promise.Promise.<int>} The value of the scrollLeft property of the element.
      */
     EyesRemoteWebElement.prototype.getScrollLeft  = function () {
-        return this._eyesDriver.executeScript(JS_GET_SCROLL_LEFT, this._element).then(function (value) {
+        return this._eyesDriver.executeScript(JS_GET_SCROLL_LEFT, this.getRemoteWebElement()).then(function (value) {
             return parseInt(value, 10);
         });
     };
@@ -199,7 +199,7 @@
      * @return {promise.Promise.<int>} The value of the scrollTop property of the element.
      */
     EyesRemoteWebElement.prototype.getScrollTop  = function () {
-        return this._eyesDriver.executeScript(JS_GET_SCROLL_TOP, this._element).then(function (value) {
+        return this._eyesDriver.executeScript(JS_GET_SCROLL_TOP, this.getRemoteWebElement()).then(function (value) {
             return parseInt(value, 10);
         });
     };
@@ -208,7 +208,7 @@
      * @return {promise.Promise.<int>} The value of the scrollWidth property of the element.
      */
     EyesRemoteWebElement.prototype.getScrollWidth  = function () {
-        return this._eyesDriver.executeScript(JS_GET_SCROLL_WIDTH, this._element).then(function (value) {
+        return this._eyesDriver.executeScript(JS_GET_SCROLL_WIDTH, this.getRemoteWebElement()).then(function (value) {
             return parseInt(value, 10);
         });
     };
@@ -217,7 +217,7 @@
      * @return {promise.Promise.<int>} The value of the scrollHeight property of the element.
      */
     EyesRemoteWebElement.prototype.getScrollHeight  = function () {
-        return this._eyesDriver.executeScript(JS_GET_SCROLL_HEIGHT, this._element).then(function (value) {
+        return this._eyesDriver.executeScript(JS_GET_SCROLL_HEIGHT, this.getRemoteWebElement()).then(function (value) {
             return parseInt(value, 10);
         });
     };
@@ -254,7 +254,7 @@
      * @return {!promise.Thenable<{width: number, height: number}>} element's size
      */
     EyesRemoteWebElement.prototype.getSize = function () {
-        return this._element.getSize().then(function (value) {
+        return this.getRemoteWebElement().getSize().then(function (value) {
             return GeometryUtils.createSize(value.width, value.height);
         });
     };
@@ -268,8 +268,8 @@
         // but we can't get raw data (including decimal values) from remote Selenium webdriver
         // and therefore we should use our own client-side script for retrieving exact values and rounding up them
 
-        // this._element.getLocation()
-        return this._eyesDriver.executeScript(JS_GET_LOCATION, this._element).then(function (value) {
+        // this.getRemoteWebElement().getLocation()
+        return this._eyesDriver.executeScript(JS_GET_LOCATION, this.getRemoteWebElement()).then(function (value) {
             var x = Math.ceil(value[0]) || 0;
             var y = Math.ceil(value[1]) || 0;
             return GeometryUtils.createLocation(x, y);
@@ -282,14 +282,14 @@
      * @return {promise.Promise.<void>}
      */
     EyesRemoteWebElement.prototype.scrollTo = function (location) {
-        return this._eyesDriver.executeScript(JS_SCROLL_TO_FORMATTED_STR(location.x, location.y), this._element);
+        return this._eyesDriver.executeScript(JS_SCROLL_TO_FORMATTED_STR(location.x, location.y), this.getRemoteWebElement());
     };
 
     /**
      * @return {promise.Promise.<string>} The overflow of the element.
      */
     EyesRemoteWebElement.prototype.getOverflow = function () {
-        return this._eyesDriver.executeScript(JS_GET_OVERFLOW, this._element);
+        return this._eyesDriver.executeScript(JS_GET_OVERFLOW, this.getRemoteWebElement());
     };
 
     /**
@@ -297,13 +297,17 @@
      * @return {promise.Promise.<void>} The overflow of the element.
      */
     EyesRemoteWebElement.prototype.setOverflow = function (overflow) {
-        return this._eyesDriver.executeScript(JS_SET_OVERFLOW_FORMATTED_STR(overflow), this._element);
+        return this._eyesDriver.executeScript(JS_SET_OVERFLOW_FORMATTED_STR(overflow), this.getRemoteWebElement());
     };
 
     /**
      * @return {WebElement} The original element object
      */
     EyesRemoteWebElement.prototype.getRemoteWebElement = function () {
+        if (this._element.getRemoteWebElement) {
+            return this._element.getRemoteWebElement();
+        }
+
         return this._element;
     };
 
