22029a3f26fff059677b5c7fa81ba52b203cad42
Oleh Astappiev <astappev@gmail.com>
2018-03-08 14:33:25 +0100

    Add tests

---
+++ test/protractor/simple-protractor-test.js
@@ -0,0 +1,47 @@
+var Eyes = require('../../index').Eyes;
+var ConsoleLogHandler = require('../../index').ConsoleLogHandler;
+var FixedCutProvider = require('../../index').FixedCutProvider;
+
+var eyes;
+
+describe("Eyes.Selenium.JavaScript - Protractor", function() {
+
+    beforeAll(function(){
+        eyes = new Eyes();
+        eyes.setApiKey(process.env.APPLITOOLS_API_KEY);
+        eyes.setLogHandler(new ConsoleLogHandler(true));
+    });
+
+    beforeEach(function(done){
+        eyes.open(browser, global.appName, global.testName, {width: 800, height: 560}).then(function () {
+            done();
+        });
+    });
+
+    it("simple protractor", function(done) {
+        browser.get("https://astappev.github.io/test-html-pages/");
+
+        eyes.addProperty("MyProp", "I'm correct!");
+
+        // cut params: header, footer, left, right.
+        eyes.setImageCut(new FixedCutProvider(60, 100, 50, 120));
+
+        eyes.checkWindow("Entire window");
+
+        element(by.name("name")).sendKeys("Test User");
+        element(by.name("email")).sendKeys("username@example.com");
+        element(by.id("submit-form")).click();
+
+        eyes.checkWindow("Entire window with cut borders");
+
+        eyes.close().then(function () {
+            done();
+        });
+    });
+
+    afterEach(function(done) {
+        eyes.abortIfNotClosed().then(function () {
+            done();
+        });
+    });
+});
